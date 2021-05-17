60d483995332dd03bdf78f54f5cd836ad276cf4e
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2021-05-17 10:23:12 +0200

    Update index.js

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,1 +1,5 @@
+randomNumber1 = Math.floor(Math.random()*6)+1
+randomNumber2 = Math.floor(Math.random()*6)+1
 
+document.querySelectorAll('img')[0].setAttribute('src', 'images/dice'+randomNumber1+'.png')
+document.querySelectorAll('img')[1].setAttribute('src', 'images/dice'+randomNumber2+'.png')
